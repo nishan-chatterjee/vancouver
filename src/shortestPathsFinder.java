79c02531017cfd4c1063145d062d5417179fe4a5
Hi-Im-Jony <jccicai@yahoo.ie>
2021-04-28 14:26:55 +0100

    re-arranged project structure

---
+++ src/shortestPathsFinder.java
@@ -0,0 +1,8 @@
+/*
+    Class to find shortest paths between 2 bus stops (as input by the user), returning the list of stops
+    en route as well as the associated "cost" 
+*/
+
+public class shortestPathsFinder {
+    
+}
---
+++ src/tripFinder.java
@@ -0,0 +1,10 @@
+
+/*
+    Searching for all trips with a given arrival time, returning full details of all trips matching the criteria (zero, one or more), 
+    sorted by trip id
+*/
+
+
+public class tripFinder {
+    
+}
