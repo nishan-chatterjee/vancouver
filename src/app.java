c772a2a1a5a2338f4b7191fbaa2f2d38b5032635
Terlo <57776535+Terlo@users.noreply.github.com>
2021-04-28 20:41:34 +0100

    Update app.java

--- src/app.java
+++ src/app.java
@@ -6,11 +6,7 @@ import java.util.Arrays;
 
 public class app {
     public static void main(String[] args) {
-        // stopFinder obj = new stopFinder();
-        String[] arr = stopFinder.getStopByArival("05:25:00");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+
     }
 
 }
